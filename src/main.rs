415038eceb193bf717648f3997f8b7c62f1d5336
Arturo <41492101+Arti-Code@users.noreply.github.com>
2022-11-17 15:34:03 +0100

    v0.3.7

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#[allow(dead_code)]
-
 use anyhow::Result;
 use tokio::time::sleep;
 //use std::str::FromStr;
@@ -88,144 +86,156 @@ impl Application {
     }
 }
 
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    let mut run: bool = true;
+    //let mut rerun: bool = false;
     let app: Application = create_application().await;
     let device: Device = create_device("kamera").await;
-    intro(&app, &device).await;
+    intro(&app).await;
+    let listener = UdpSocket::bind("127.0.0.1:5004").await.unwrap();
+    let list = Arc::new(listener);
+    loop {
+        restart_info(&device).await;
+        let mut m = MediaEngine::default();
+        m.register_default_codecs()?;
+        let mut registry = Registry::new();
+        registry = register_default_interceptors(registry, &mut m)?;
 
-    //let identify: String=String::from_str(&"kamera").unwrap();
-    //println!("device: {}", identify);
-    
+        let api = APIBuilder::new()
+            .with_media_engine(m)
+            .with_interceptor_registry(registry)
+            .build();
+        
+        let config = RTCConfiguration {
+            ice_servers: vec![RTCIceServer {
+                urls: vec!["stun:fr-turn1.xirsys.com".to_owned()],
+                username: "23Xgr3XVCOk2GqoZW5eWhbdXM1EfA8VcC6OVVacJSpFdoljTUOsTcgAoFUvfN4vcAAAAAGNFN29nd296ZHlrMg==".to_owned(),
+                credential: "2ed490ce-4947-11ed-bd3d-0242ac120004".to_owned(),
+                credential_type: RTCIceCredentialType::Password.to_owned()
+            }],
+            ..Default::default()
+        };
 
-    let mut m = MediaEngine::default();
-    m.register_default_codecs()?;
-    let mut registry = Registry::new();
-    registry = register_default_interceptors(registry, &mut m)?;
+        let peer_connection = Arc::new(api.new_peer_connection(config).await?);
+        
+        let video_track = Arc::new(TrackLocalStaticRTP::new(
+            RTCRtpCodecCapability {mime_type: MIME_TYPE_VP8.to_owned(),
+                ..Default::default()}, "video".to_owned(), "webrtc-rs".to_owned(), ));
+        let rtp_sender = peer_connection
+            .add_track(Arc::clone(&video_track) as Arc<dyn TrackLocal + Send + Sync>).await?;
 
-    let api = APIBuilder::new()
-        .with_media_engine(m)
-        .with_interceptor_registry(registry)
-        .build();
+        tokio::spawn(async move {
+            let mut rtcp_buf = vec![0u8; 1500];
+            while let Ok((_, _)) = rtp_sender.read(&mut rtcp_buf).await {}
+            Result::<()>::Ok(())
+        });
 
-    let config = RTCConfiguration {
-        ice_servers: vec![RTCIceServer {
-            urls: vec!["stun:fr-turn1.xirsys.com".to_owned()],
-            username: "23Xgr3XVCOk2GqoZW5eWhbdXM1EfA8VcC6OVVacJSpFdoljTUOsTcgAoFUvfN4vcAAAAAGNFN29nd296ZHlrMg==".to_owned(),
-            credential: "2ed490ce-4947-11ed-bd3d-0242ac120004".to_owned(),
-            credential_type: RTCIceCredentialType::Password.to_owned()
-        }],
-        ..Default::default()
-    };
-    
-    let peer_connection = Arc::new(api.new_peer_connection(config).await?);
-    let video_track = Arc::new(TrackLocalStaticRTP::new(
-        RTCRtpCodecCapability {mime_type: MIME_TYPE_VP8.to_owned(),
-            ..Default::default()}, "video".to_owned(), "webrtc-rs".to_owned(), ));
-    let rtp_sender = peer_connection
-        .add_track(Arc::clone(&video_track) as Arc<dyn TrackLocal + Send + Sync>).await?;
+        let (done_tx, mut done_rx) = tokio::sync::mpsc::channel::<()>(1);
+        let done_tx1 = done_tx.clone();
+        let (_data_tx, mut _data_rx) = tokio::sync::mpsc::channel::<()>(1);
 
-    tokio::spawn(async move {
-        let mut rtcp_buf = vec![0u8; 1500];
-        while let Ok((_, _)) = rtp_sender.read(&mut rtcp_buf).await {}
-        Result::<()>::Ok(())
-    });
+        peer_connection
+            .on_data_channel(Box::new(move |d: Arc<RTCDataChannel>| {
+                let d_label = d.label().to_owned();
+                //let d_id = d.id();
+                println!("[NEW DATACHANNEL]: {}", d_label);
 
-    let (done_tx, mut done_rx) = tokio::sync::mpsc::channel::<()>(1);
-    let done_tx1 = done_tx.clone();
-    let (_data_tx, mut _data_rx) = tokio::sync::mpsc::channel::<()>(1);
+                Box::pin(async move {
+                    let _d2 = Arc::clone(&d);
+                    let d_label2 = d_label.clone();
+                    //let d_id2 = d_id;
+                    d.on_open(Box::new(move || {
+                        println!("[DATACHANNEL OPEN]: {}", d_label2);
+                        Box::pin(async move {})
+                    })).await;
 
-    peer_connection
-        .on_data_channel(Box::new(move |d: Arc<RTCDataChannel>| {
-            let d_label = d.label().to_owned();
-            let d_id = d.id();
-            println!("[NEW DATACHANNEL]: {} {}", d_label, d_id);
+                    d.on_message(Box::new(move |msg: DataChannelMessage| {
+                        let msg_str = String::from_utf8(msg.data.to_vec()).unwrap();
+                        println!("[↓]: {}", msg_str);
+                        Box::pin(async {})
+                    })).await;
+                })
+            })).await;
 
-            Box::pin(async move {
-                let _d2 = Arc::clone(&d);
-                let d_label2 = d_label.clone();
-                let d_id2 = d_id;
-                d.on_open(Box::new(move || {
-                    println!("[DATACHANNEL] {}<{}> open", d_label2, d_id2);
-                    Box::pin(async move {})
-                })).await;
-
-                d.on_message(Box::new(move |msg: DataChannelMessage| {
-                    let msg_str = String::from_utf8(msg.data.to_vec()).unwrap();
-                    println!("[↓]: {}", msg_str);
-                    Box::pin(async {})
-                })).await;
-            })
-        })).await;
-
-    peer_connection
-        .on_ice_connection_state_change(Box::new(move |connection_state: RTCIceConnectionState| {
-            println!("Connection State has changed {}", connection_state);
-            if connection_state == RTCIceConnectionState::Failed {
-                let _ = done_tx1.try_send(());
-            }
-            Box::pin(async {})
-        })).await;
-
-    let done_tx2 = done_tx.clone();
-
-    peer_connection
-        .on_peer_connection_state_change(Box::new(move |s: RTCPeerConnectionState| {
-            println!("Peer Connection State has changed: {}", s);
-            if s == RTCPeerConnectionState::Failed {
-                // Wait until PeerConnection has had no network activity for 30 seconds or another failure. It may be reconnected using an ICE Restart.
-                // Use webrtc.PeerConnectionStateDisconnected if you are interested in detecting faster timeout.
-                // Note that the PeerConnection may come back from PeerConnectionStateDisconnected.
-                println!("Peer Connection has gone to failed exiting: Done forwarding");
-                let _ = done_tx2.try_send(());
-            }
-            Box::pin(async {})
-        })).await;
-    
-    let offer_encoded = wait_offer(device.get_name()).await;
-
-    let desc_data = signal::decode(&offer_encoded)?;
-    let offer = serde_json::from_str::<RTCSessionDescription>(&desc_data)?;
-    peer_connection.set_remote_description(offer).await?;
-    let answer = peer_connection.create_answer(None).await?;
-
-    let mut gather_complete = peer_connection.gathering_complete_promise().await;
-    peer_connection.set_local_description(answer).await?;
-    let _ = gather_complete.recv().await;
-
-    if let Some(local_desc) = peer_connection.local_description().await {
-        send_answer(&local_desc, device.get_name()).await;
-    } else {
-        println!("generate local_description failed!");
-    }
-
-    let listener = UdpSocket::bind("127.0.0.1:5004").await?;
-    let done_tx3 = done_tx.clone();
-    tokio::spawn(async move {
-        let mut inbound_rtp_packet = vec![0u8; 1600]; // UDP MTU
-        while let Ok((n, _)) = listener.recv_from(&mut inbound_rtp_packet).await {
-            if let Err(err) = video_track.write(&inbound_rtp_packet[..n]).await {
-                if Error::ErrClosedPipe == err {
-                } else {
-                    println!("video_track write err: {}", err);
+        
+            peer_connection
+            .on_ice_connection_state_change(Box::new(move |connection_state: RTCIceConnectionState| {
+                println!("[ICE CONNECTION]: {}", connection_state);
+                if connection_state == RTCIceConnectionState::Disconnected {
+                    let _ = done_tx1.try_send(());
                 }
-                let _ = done_tx3.try_send(());
-                return;
+                if connection_state == RTCIceConnectionState::Failed {
+                    let _ = done_tx1.try_send(());
+                }
+                Box::pin(async {})
+            })).await;
+
+        let done_tx2 = done_tx.clone();
+
+        peer_connection
+            .on_peer_connection_state_change(Box::new(move |s: RTCPeerConnectionState| {
+                println!("[PEER CONNECTION]: {}", s);
+                if s == RTCPeerConnectionState::Failed {
+                    // Wait until PeerConnection has had no network activity for 30 seconds or another failure. It may be reconnected using an ICE Restart.
+                    // Use webrtc.PeerConnectionStateDisconnected if you are interested in detecting faster timeout.
+                    // Note that the PeerConnection may come back from PeerConnectionStateDisconnected.
+                    let _ = done_tx2.try_send(());
+                }
+                Box::pin(async {})
+            })).await;
+        
+        
+        let offer_encoded = wait_offer(device.get_name()).await;
+
+        let desc_data = signal::decode(&offer_encoded)?;
+        let offer = serde_json::from_str::<RTCSessionDescription>(&desc_data)?;
+        peer_connection.set_remote_description(offer).await?;
+        let answer = peer_connection.create_answer(None).await?;
+
+        let mut gather_complete = peer_connection.gathering_complete_promise().await;
+        peer_connection.set_local_description(answer).await?;
+        let _ = gather_complete.recv().await;
+
+        if let Some(local_desc) = peer_connection.local_description().await {
+            send_answer(&local_desc, device.get_name()).await;
+        } else {
+            println!("[ANSWER]: failed!");
+        }
+        let done_tx3 = done_tx.clone();
+        
+        let l = Arc::clone(&list);
+        tokio::spawn(async move {
+            let mut inbound_rtp_packet = vec![0u8; 1600]; // UDP MTU
+            while let Ok((n, _)) = l.recv_from(&mut inbound_rtp_packet).await {
+                if let Err(err) = video_track.write(&inbound_rtp_packet[..n]).await {
+                    if Error::ErrClosedPipe == err {
+                    } else {
+                        println!("[VIDEO ERROR]: {}", err);
+                    }
+                    let _ = done_tx3.try_send(());
+                    return;
+                }
             }
-        }
-    });
+        });
 
-    println!("Press ctrl-c to stop");
-    tokio::select! {
-        _ = done_rx.recv() => {
-            println!("received done signal!");
+        tokio::select! {
+            _ = done_rx.recv() => {
+                println!("[SIGNAL]: done");
+            }
+            _ = tokio::signal::ctrl_c() => {
+                println!("");
+                run = false;
+            }
+        };
+        peer_connection.close().await?;
+        if !run {
+            break;
         }
-        _ = tokio::signal::ctrl_c() => {
-            println!("");
-        }
-    };
-
-    peer_connection.close().await?;
+        //rerun = true;
+        //listener = UdpSocket::bind("127.0.0.1:9004").await.unwrap();
+    }
     Ok(())
 }
 
@@ -254,7 +264,7 @@ async fn create_application() -> Application {
     app
 }
 
-async fn intro(app: &Application, device: &Device) {
+async fn intro(app: &Application) {
     println!("{}", app.get_name());
     wait(1).await;
     println!("version {}", app.get_version());
@@ -265,10 +275,13 @@ async fn intro(app: &Application, device: &Device) {
     wait(1).await;
     println!("{}", app.get_description());
     wait(3).await;
+}
+
+async fn restart_info(device: &Device) {
     println!("device {}", device.get_name());
     wait(1).await;
     println!("connecting...");
-    wait(1).await;
+    wait(3).await;
 }
 
 async fn wait_offer(device: &str) -> String {
@@ -291,7 +304,7 @@ async fn wait_offer(device: &str) -> String {
                 firebase2.update(&clear_offer).await.unwrap();
             },
             Ok(_) => {
-                sleep(Duration::from_secs(3)).await;
+                sleep(Duration::from_secs(10)).await;
             },
             Err(_) => {
                 sleep(Duration::from_secs(3)).await;
